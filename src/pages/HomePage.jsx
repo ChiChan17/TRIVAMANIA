b10d8e2f0585d4b035494cd92165b63873662803
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2024-02-07 17:20:13 -0800

    Aesthetic changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,12 +6,13 @@ import { Link } from 'react-router-dom';
 import { AwesomeButton } from 'react-awesome-button';
 import 'react-awesome-button/dist/styles.css';
 import { GearIcon } from "@primer/octicons-react"; // custom icons
+import ParticlesBg from 'particles-bg';
 
 const HomePage = () => {
 
   return (
     <div className = 'homepage'>
-
+      <ParticlesBg color="#ff0000" type="circle" bg={true}/>
       <div className='button-container'>
           <Link to="/SettingPage">    
             <AwesomeButton id="settings-button" type="danger">
